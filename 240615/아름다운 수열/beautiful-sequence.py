83e3c7c9cf3a92f5ab3c4087b18c596c7b156e8a
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-15 18:49:15 +0900

    Title: 아름다운 수열, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240615/아름다운 수열/beautiful-sequence.py
+++ 240615/아름다운 수열/beautiful-sequence.py
@@ -12,21 +12,21 @@ M_list.sort()
 K = 0
 K_idx = []
 p = []
-if len(M_list) == 1:
-    K = len(N_list)
-    K_idx.extend([idx_k+1 for idx_k in range(len(N_list))])
-else:
-    for idx in range(0, len(N_list)-len(M_list)+1):
-        temp_p = sorted(N_list[idx:idx+3])
-        diff = M_list[0]-temp_p[0]
-        flag = True
-        for idx_m in range(len(M_list)):
-            if diff != M_list[idx_m]-temp_p[idx_m]:
-                flag = False
-                break
-        if flag:
-            K += 1
-            K_idx.append(idx+1)
+# if len(M_list) == 1:
+#     K = len(N_list)
+#     K_idx.extend([idx_k+1 for idx_k in range(len(N_list))])
+# else:
+for idx in range(0, len(N_list)-len(M_list)+1):
+    temp_p = sorted(N_list[idx:idx+len(M_list)])
+    diff = M_list[0]-temp_p[0]
+    flag = True
+    for idx_m in range(len(M_list)-1):
+        if diff != M_list[idx_m]-temp_p[idx_m]:
+            flag = False
+            break
+    if flag:
+        K += 1
+        K_idx.append(idx+1)
 
 print(K)
 for idx_k in K_idx:
