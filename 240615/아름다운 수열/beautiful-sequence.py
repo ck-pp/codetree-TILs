903c0d1cdec09c44330a75d12de1ca8148f68a0e
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-15 18:45:13 +0900

    Title: 아름다운 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240615/아름다운 수열/beautiful-sequence.py
+++ 240615/아름다운 수열/beautiful-sequence.py
@@ -2,29 +2,31 @@ N = int(input())
 N_list = []
 for _ in range(N):
     N_list.append(int(input()))
-# 1 8 5 7 9 10
 
 M = int(input())
 M_list = []
 for _ in range(M):
     M_list.append(int(input()))
 M_list.sort()
-# 4 6 7
 
 K = 0
 K_idx = []
 p = []
-for idx in range(0, len(N_list)-len(M_list)+1):
-    temp_p = sorted(N_list[idx:idx+3])
-    diff = M_list[0]-temp_p[0]
-    flag = True
-    for idx_m in range(len(M_list)):
-        if diff != M_list[idx_m]-temp_p[idx_m]:
-            flag = False
-            break
-    if flag:
-        K += 1
-        K_idx.append(idx+1)
+if len(M_list) == 1:
+    K = len(N_list)
+    K_idx.extend([idx_k+1 for idx_k in range(len(N_list))])
+else:
+    for idx in range(0, len(N_list)-len(M_list)+1):
+        temp_p = sorted(N_list[idx:idx+3])
+        diff = M_list[0]-temp_p[0]
+        flag = True
+        for idx_m in range(len(M_list)):
+            if diff != M_list[idx_m]-temp_p[idx_m]:
+                flag = False
+                break
+        if flag:
+            K += 1
+            K_idx.append(idx+1)
 
 print(K)
 for idx_k in K_idx:
